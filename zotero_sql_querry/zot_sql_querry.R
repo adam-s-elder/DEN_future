657e6b7229e12dba4c5c259e244c5d71176551a8
adam-s-elder <adam.elder.stat@gmail.com>
2025-02-25 14:39:02 -0800

    Removing duplicate items and adding parameter type (digital versus manual).

--- zotero_sql_querry/zot_sql_querry.R
+++ zotero_sql_querry/zot_sql_querry.R
@@ -19,12 +19,29 @@ dt_names <- dbListTables(portaldb)
 name_list <- dt_names |> map(.f = function(df_name) {
   collect(tbl(portaldb, df_name)) |> colnames()
 })
+names(name_list) <- dt_names
+name_list |> keep(
+  .p = function(x) any(grepl("itemID", x))
+)
+
 param_dictionary <-
   map2(name_list, dt_names, .f = function(x, y){
   bind_cols(param = x, dt_name = y)
 }) |> do.call(what = rbind)
+
 dbListFields(portaldb, "itemNotes")
+dbListFields(portaldb, "libraries")
 dbListFields(portaldb, "tags")
+
+# Libraries
+collection_table <- collect(tbl(portaldb, "collections"))
+collection_items_table <- collect(tbl(portaldb, "collectionItems"))
+sr_include_collection_id <-
+  collection_table |> filter(collectionName == "sr_included_articles") |>
+  pull(collectionID)
+items_to_keep <-
+  collection_items_table |> filter(collectionID == sr_include_collection_id) |>
+  pull(itemID)
 
 annotation_table_df <- collect(tbl(portaldb, "itemAnnotations"))
 annotation_table_df <- annotation_table_df |> mutate(
@@ -32,39 +49,52 @@ annotation_table_df <- annotation_table_df |> mutate(
   param_value = stringr::str_remove_all(param_value, "\\{|\\}")
 )
 
-
 # Tag data table
 tag_idx <- collect(tbl(portaldb, "tags"))
 item_tags <- collect(tbl(portaldb, "itemTags"))
-collect(tbl(portaldb, "tags")) |> View()
+item_tag_names <- item_tags |> left_join(tag_idx, by = "tagID")
 
 annotations_and_tags <-
-  left_join(item_tags,
+  left_join(item_tag_names,
             annotation_table_df |> select(-type), by = "itemID") |>
   filter(!is.na(comment))
 
-annotations_and_tags <-
-  annotations_and_tags |> left_join(tag_idx, by = "tagID")
+# annotations_and_tags <-
+#   annotations_and_tags |> left_join(tag_idx, by = "tagID")
 
 # Find note's parent items
 attachments_table_df <- collect(tbl(portaldb, "itemAttachments"))
 attachment_ids <- annotations_and_tags$parentItemID |> unique()
-annotations_and_tags <- left_join(annotations_and_tags,
-          attachments_table_df |> filter(itemID %in% attachment_ids) |>
-            rename(articleItemID = parentItemID,
-                   parentItemID = itemID),
-          by = "parentItemID")
+# Overall, there are annotations with tags and each annotation is made within
+# a given pdf.  The tags are applied to the annotations, so we have the
+# following hierarchy: tag <- annotation <- pdf <- journal article.
 
-parameter_item_idx <- annotations_and_tags |> filter(name == "parameter") |>
+# One confusion is that annotations, pdfs, and articles are all "items"
+# Attachments here are the pdfs and their parents are the articles.  We
+# rename the itemID to parentItemID because the pdfs are the parents of the
+# annotations.  The tags associated with each annotation have tagIDs.
+
+annotation_parent_df <- attachments_table_df |>
+  filter(itemID %in% attachment_ids) |>
+  rename(articleItemID = parentItemID,
+         parentItemID = itemID)
+
+# Tags with information on the annotation ID (itemID) and
+# article ID (articleItemID)
+tag_df_w_aa <- left_join(annotations_and_tags, annotation_parent_df,
+          by = "parentItemID") |>
+  filter(articleItemID %in% items_to_keep)
+
+parameter_item_idx <- tag_df_w_aa |> filter(name == "parameter") |>
   pull(itemID)
 
-parameter_df <- annotations_and_tags |>
+parameter_df <- tag_df_w_aa |>
   filter(itemID %in% parameter_item_idx) |>
   filter(name != "parameter")
 
-parameter_notes_df <- full_join(
-  item_tags |> filter(itemID %in% parameter_item_idx),
-  parameter_df, relationship = "many-to-many", by = "itemID")
+# parameter_notes_df <- full_join(
+#   item_tags |> filter(itemID %in% parameter_item_idx),
+#   parameter_df, relationship = "many-to-many", by = "itemID")
 
 # Getting article title / names
 item_data_df <- collect(tbl(portaldb, "itemData"))
@@ -81,7 +111,7 @@ wanted_field_value_df <-
   left_join(value_id_df, item_data_values_df, by = "valueID")
 
 parameter_notes_df <- left_join(
-  parameter_notes_df,
+  parameter_df,
   wanted_field_value_df |> rename(articleItemID = itemID),
   by = "articleItemID")
 
@@ -102,10 +132,23 @@ if (length(non_match) > 0) {
           paste0(non_match, collapse = ", "))
 }
 
+en_type <- parameter_notes_df |> filter(name %in% c("manual", "digital")) |>
+  select(name, itemID)
+
+missed_item_ids <- en_type |> group_by(itemID) |> count() |> filter(n > 1) |>
+  pull(itemID)
+stop_message <- "The following item IDs (annotations) have been associated with both manual and digital contact tracing:\n"
+if (length(missed_item_ids) > 0) stop(paste0(c(stop_message, missed_item_ids), collapse = "\n"))
+
+parameter_notes_df <-
+  parameter_notes_df |> filter(!(name %in% c("manual", "digital"))) |>
+  left_join(en_type |> select(itemID, param_type = name), by = "itemID")
+
 simple_param_df <- parameter_notes_df |> select(
   param_name = name,
   all_of(param_order),
-  source = value
+  source = value,
+  param_type
 ) |> filter(!is.na(param_value))
 
 write.csv(simple_param_df, file = "simplified_parameter_df.csv")
